364f11684bc5c0d2aeac8f97978bc7f623d5f4e5
yyubin <hazing120@gmail.com>
2025-01-16 14:24:24 +0900

    refactor: canvas insert -> upsert

--- repository/canvas_repository.go
+++ repository/canvas_repository.go
@@ -7,6 +7,7 @@ import (
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type CanvasRepository struct {
@@ -19,7 +20,12 @@ func NewCanvasRepository(collection *mongo.Collection) *CanvasRepository {
 
 func (r *CanvasRepository) SaveCanvas(canvas models.Canvas) error {
 	canvas.CreatedAt = time.Now()
-	_, err := r.collection.InsertOne(context.Background(), canvas)
+	filter := bson.M{"team_id": canvas.TeamID}
+	update := bson.M{
+		"$set": canvas,
+	}
+	opts := options.Update().SetUpsert(true)
+	_, err := r.collection.UpdateOne(context.Background(), filter, update, opts)
 	return err
 }
 
